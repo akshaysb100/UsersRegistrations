bc14d87efe1561c945f1f25f8d5598ed84022bda
akshaysb100 <akshaybavalekar100@gmail.com>
2019-11-22 18:34:43 +0530

    Add:create tests cases for last name validation

--- src/test/java/com/userregistration/com/UserRegistration.java
+++ src/test/java/com/userregistration/com/UserRegistration.java
@@ -1,5 +1,6 @@
 package com.userregistration.com;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.lang.reflect.Array;
@@ -35,5 +36,61 @@ public class UserRegistration {
         }else {
             return false;
         }
+    }
+    @Test
+    public void givenLastName_WhenCorrect_ReturnTrue() {
+
+        UserRegistration userRegistration = new UserRegistration();
+        boolean lastName = userRegistration.checkLastName("Bavalekar");
+        Assert.assertTrue(lastName);
+    }
+
+    @Test
+    public void givenLastName_WhenMinimumThreeCharacter_ReturnTrue() {
+
+        UserRegistration userRegistration = new UserRegistration();
+        boolean lastName = userRegistration.checkLastName("Bavalekar");
+        Assert.assertTrue(lastName);
+    }
+
+    @Test
+    public void givenLastName_WhenLowerCase_ReturnFalse() {
+
+        UserRegistration userRegistration = new UserRegistration();
+        boolean lastName = userRegistration.checkLastName("bavalekar");
+        Assert.assertFalse(lastName);
+    }
+
+    @Test
+    public void givenLastName_WhenAllCaps_ReturnFalse() {
+
+        UserRegistration userRegistration = new UserRegistration();
+        boolean lastName = userRegistration.checkLastName("BAVALEKAR");
+        Assert.assertFalse(lastName);
+
+    }
+
+    @Test
+    public void givenLastName_WhenContainNumber_ReturnFalse() {
+
+        UserRegistration userRegistration = new UserRegistration();
+        boolean lastName = userRegistration.checkLastName("Bavalekar123");
+        Assert.assertFalse(lastName);
+    }
+
+    @Test
+    public void givenLastName_WhenSpecialCharacter_ReturnFalse() {
+
+        UserRegistration userRegistration = new UserRegistration();
+        boolean lastName = userRegistration.checkLastName("AK@SHAY");
+        Assert.assertFalse(lastName);
+    }
+
+    @Test
+    public void givenLastName_WhenMinimumTreeCharacter_ReturnFalse() {
+
+        UserRegistration userRegistration = new UserRegistration();
+        boolean lastName = userRegistration.checkLastName("Ak");
+        Assert.assertFalse(lastName);
     }
 }
